01c4ce44eb4b19d9cf8b78a10f1fadd08f02ef83
rachaelkiteme <rachaelrirrie@gmail.com>
2017-11-19 00:21:12 +0300

    add sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'users/new'
+
+  get 'users/index'
+
   get 'static_pages/about'
   root 'static_pages#home'
 
