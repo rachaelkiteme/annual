b080c73f36a474b8fdadb475598424f77f19821e
rachaelkiteme <rachaelrirrie@gmail.com>
2018-02-13 15:57:34 +0300

    Added succesful log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,13 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		redirect_to  root_url
+      flash[:success] = "Welcome to automated annual leave system"
+  		redirect_to  @user
   	else
   		render 'new'
   		
   	end
-
-   end
-  	
-
+end
   def index
   end
   def show
