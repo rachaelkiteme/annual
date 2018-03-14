19ec08044202fe722937bf64f6e0aeeffd0b91d7
rachaelkiteme <rachaelrirrie@gmail.com>
2018-03-14 16:16:18 +0300

    updated user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
 	validates :name, presence: :true
 	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-	has_secure_password
 end
